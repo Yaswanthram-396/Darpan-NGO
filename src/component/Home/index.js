ea25ab0215983d8f83bc0d56d791340924fe116c
Yaswanth <yaswanthram.396@gmail.com>
2024-08-03 14:57:43 +0530

    changes

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -1,11 +1,11 @@
-// import PageCards from "./ourProjects/ourProjects";
-// import Whoweare from "./WhoWeAre/Whoweare";
-// import CreateOurImpact from "./ourImpact/ourImpact";
-// import Testimonials from "./Testimonials/testimonials";
-// import MoveWithUs from "./moveWithUs/moveWithUs";
-// import OurGallery from "./OurGallery/ourGallery";
-// import CarouselPage from "./coursel/coursel";
-import HealthAndfamily from "../Health&familypage";
+import PageCards from "./ourProjects/ourProjects";
+import Whoweare from "./WhoWeAre/Whoweare";
+import CreateOurImpact from "./ourImpact/ourImpact";
+import Testimonials from "./Testimonials/testimonials";
+import MoveWithUs from "./moveWithUs/moveWithUs";
+import OurGallery from "./OurGallery/ourGallery";
+import CarouselPage from "./coursel/coursel";
+// import HealthAndfamily from "../Health&familypage";
 import Navbar from "./Navbar/Navbar";
 
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,14 +16,14 @@ function Home() {
   return (
     <div className="App">
       <Navbar />
-      <HealthAndfamily />
-      {/* <CarouselPage />
+      {/* <HealthAndfamily /> */}
+      <CarouselPage />
       <PageCards />
       <Whoweare />
       <CreateOurImpact />
       <Testimonials />
       <MoveWithUs />
-      <OurGallery /> */}
+      <OurGallery />
     </div>
   );
 }
